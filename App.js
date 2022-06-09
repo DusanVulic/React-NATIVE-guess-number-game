60429bb9b5ab9dfde492fa878e7a142eae320c48
Dusan <dusanvulic11@gmail.com>
2022-06-09 19:17:42 +0200

    screens and custom buttons created

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+//import screens
+import StartGameScreen from "./screens/StartGameScreen";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello WORLD !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#495af3",
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
