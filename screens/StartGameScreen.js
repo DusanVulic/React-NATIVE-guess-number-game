a1158b90045dc29cc2bc5a999f355a4174102414
Dusan <dusanvulic11@gmail.com>
2022-06-09 19:32:35 +0200

    adding styles for android and ios

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,10 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 //import custom created buttons
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -13,3 +13,20 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    // box shaddow for android
+    elevation: 4,
+    //box shaddow for IOS
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 6 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
